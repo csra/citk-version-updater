1bfe974b521b6e5ad17aef2d4f0f0824c02214fa
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2016-07-15 14:46:17 +0200

    cleanup project structure

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 setup (
-       name = 'CitkProjectUpdater',
-       version = '0.1',
-       packages = find_packages(),
+       name='CitkProjectUpdater',
+       version='0.1',
+       packages=find_packages(),
 
        # Fill in these to make your Egg ready for upload to
        # PyPI
-       author = 'DivineThreepwood',
-       author_email = 'divine@openbase.org',
+       author='DivineThreepwood',
+       author_email='divine@openbase.org',
 
        #summary = 'Just another Python package for the cheese shop',
-       url = 'http://www.openbase.org',
-       license = '',
-       long_description = 'Upgrade tool for citk projects. Project tags are synchronized with the remote repository and the latest release can be configured for a given distribution.',
+       url='http://www.openbase.org',
+       license="LGPLv3",
+       long_description='Upgrade tool for citk projects. Project tags are synchronized with the remote repository and the latest release can be configured for a given distribution.',
+       
 
+       scripts=['citec.citk/citk-version-updater.py'],
        # could also include long_description, download_url, classifiers, etc.
        
        # Declare your packages' dependencies here, for eg:
